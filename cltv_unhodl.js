f70ae4a38aa830686248a92fdeb9708b6baf7284
Alexander Petrov <alpet1@gmail.com>
2025-04-21 12:23:51 +0300

    Little refactoring
    Improved log output

--- cltv_unhodl.js
+++ cltv_unhodl.js
@@ -1,146 +1,145 @@
-  /**
-   * You have to run this script twice!
-   * After first run: take note the lockTime value and send coins to the P2SH address
-   * Before second run: replace TX_ID, TX_VOUT and locktime
-   * Generate 20 blocks
-   * Send transaction
-   * */
+    /**
+     * You have to run this script twice!
+     * After first run: take note the lockTime value and send coins to the P2SH address
+     * Before second run: replace TX_ID, TX_VOUT and locktime
+     * Generate 20 blocks
+     * Send transaction
+     * */
 
-  //
-  const { execSync } = require('child_process');
-  const bitcoin = require('bitcoinjs-lib');
-  const colors = require('colors');
-  const ECPairFactory  = require('ecpair');
-  const ecc = require('tiny-secp256k1');
-  // const network = bitcoin.networks.regtest
-  const network = bitcoin.networks.bitcoin
-  const hashType = bitcoin.Transaction.SIGHASH_ALL
-  const fs = require('fs')
-  const args = process.argv.slice(2);
-  const Buffer = require('safe-buffer').Buffer;
-  const psbtutils = require('bitcoinjs-lib/src/psbt/psbtutils');
-  const util = require('util');
+    //
+    const { execSync } = require('child_process');
+    const bitcoin = require('bitcoinjs-lib');
+    const colors = require('colors');
+    const ECPairFactory  = require('ecpair');
+    const ecc = require('tiny-secp256k1');
+    // const network = bitcoin.networks.regtest
+    const network = bitcoin.networks.bitcoin
+    const hashType = bitcoin.Transaction.SIGHASH_ALL
+    const fs = require('fs')
+    const args = process.argv.slice(2);
+    const Buffer = require('safe-buffer').Buffer;
+    const psbtutils = require('bitcoinjs-lib/src/psbt/psbtutils');
+    const util = require('util');
 
-  // WARNING: need receiver privkey & pubkey pair!
-  if (!fs.existsSync('./hodlmaster_key.json')) {
-    console.log('#FATAL: Not found private key file!');
-    return;
-  }
-
-  const ECPair = ECPairFactory.ECPairFactory(ecc);  
-  // Transaction signer
-  const privKey = require('./hodlmaster_key.json').toString();
-  const keyPair = ECPair.fromWIF(privKey, network)
-
-  // Replace the lockTime value on second run here!
-  buff = ''
-
-  cfname = './config.json';
-  const config = require(cfname);
-    
-  if (0 == config.lockTime || "" == config.txid) {
-    console.log('#ERROR: config.json not ready for tx generation');  
-    return;
-  }
-
-  dt = new Date(config.lockTime * 1000);
-  const hodl_ts = dt.toUTCString();
-
-  console.log('CHECK CAREFULLY: Timelock in UNIX timestamp for '.brightYellow + hodl_ts.brightGreen + ' = '.brightWhite + config.lockTime.toString().brightMagenta);
-
-  // Generate witness script and P2WSH address
-  if (!config.witness_script) {
-    console.log("#ERROR: not specified witness script in config");
-    return; 
-  }
-  const ws_buff = Buffer.from(config.witness_script, 'hex');
-  const p2wsh = bitcoin.payments.p2wsh({redeem: {output: ws_buff, network}, network})
-  const redeemScript = p2wsh.redeem.output;
-
-  // there coins can be hodl over years...
-  if (config.hodl_addr != p2wsh.address) {
-    console.log('#ERROR: mistmatch hold_addr, generated ['.brightRed + p2wsh.address + '] vs saved ['.brightRed + config.hodl_addr + ']'.brightRed);
-    return;
-  }
-
-
-  const psbt = new bitcoin.Psbt( { network:network } );
-  psbt.setLocktime(config.lockTime);
-
-  psbt.addInput( { 
-      hash:config.txid, 
-      index:config.vout,   // 0 for first hodl tx, 1 for second... 
-      sequence:0xfffffffe,
-      witnessScript: ws_buff,
-      nonWitnessUtxo: Buffer.from(config.tx_hex, 'hex'),
-      redeemScript
-  });
-
-
-  var receiver = 'bc1qa5judde8fftgu92rv29vlelw0dpr66nh0wdnut'; // alpet donation addr
-  if (args[0])
-    receiver = args[0]; // override receiver
-  else
-    console.log("#WARN: using embedded receiver address ".brightRed + receiver.brightWhite);
-
-  var totalInput = config.total_hodl; // 1BTC
-
-  if (args[1])
-    totalInput = parseFloat(args[1]);
-
-  totalInput = Math.floor(totalInput * 100000000); // convert to Satoshis
-  var totalOutput = totalInput - 27000; // WARN: specify yours fee here
-
-  console.log ("#WARN: totalInput used ".red + totalInput.toString().brightMagenta + " sat.!".red);
-
-  psbt.addOutput({address:receiver, value:totalOutput}) // to whom and how much money to send?
-
-  const wsDump = execSync('bitcoin-cli decodescript ' + config.witness_script);
-  const pubkey = Buffer.from(keyPair.publicKey);  
-  const pubkey_hex = pubkey.toString('hex');
-  console.log("#WITNESS_SCRIPT: ".brightYellow + wsDump);    
-  if (wsDump.includes(pubkey_hex))
-      console.log("#OK: publicKey used in script".brightGreen);
-  else {
-      console.log("#FATAL: publicKey not used in script ".brightRed + pubkey_hex.brightWhite);
-      console.log(keyPair);
+    // WARNING: need receiver privkey & pubkey pair!
+    if (!fs.existsSync('./hodlmaster_key.json')) {
+      console.log('#FATAL: Not found private key file!');
       return;
-  }
-    
-  const signer = {
-      network: network,
-      publicKey: pubkey,      
-      sign: ($hash) => {   
-        return Buffer.from(keyPair.sign($hash)); 
-      },
-  };
+    }
 
-  console.log("#DBG: trying sign input...");  
-  psbt.signInput(0, signer);
+    const ECPair = ECPairFactory.ECPairFactory(ecc);  
+    // Transaction signer
+    const privKey = require('./hodlmaster_key.json').toString();
+    const keyPair = ECPair.fromWIF(privKey, network)
 
-  const finalizeInput = (_inputIndex, input ) => {
-    const signature = Buffer.from(input.partialSig[0].signature);
-    console.log("#SIGNATURE: " + signature);
+    // Replace the lockTime value on second run here!
+    buff = ''
 
-    const redeemPayment = bitcoin.payments.p2wsh({
-        redeem: {
-          input: bitcoin.script.compile([signature]),
-          output: input.witnessScript
-        }
-      });
+    cfname = './config.json';
+    const config = require(cfname);
+      
+    if (0 == config.lockTime || "" == config.txid) {
+        console.log("#ERROR: ".brightRed + ' config.json not ready for tx generation'.brightWhite);  
+        return;
+    }
 
-      const result = psbtutils.witnessStackToScriptWitness (redeemPayment.witness ?? []);
-      return { finalScriptSig: Buffer.from(""), 
-              finalScriptWitness:result }
-  }
+    dt = new Date(config.lockTime * 1000);
+    const hodl_ts = dt.toUTCString();
 
-  psbt.finalizeInput(0, finalizeInput);
+    console.log('CHECK CAREFULLY: Timelock in UNIX timestamp for '.brightYellow + hodl_ts.brightGreen + ' = '.brightWhite + config.lockTime.toString().brightMagenta);
 
-  const txraw = psbt.extractTransaction().toHex();
-  console.log('TXRAW="' + txraw.brightWhite + '"');
+    // Generate witness script and P2WSH address
+    if (!config.witness_script) {
+        console.log("#ERROR: not specified witness script in config");
+        return; 
+    }
+    const ws_buff = Buffer.from(config.witness_script, 'hex');
+    const p2wsh = bitcoin.payments.p2wsh({redeem: {output: ws_buff, network}, network})
+    const redeemScript = p2wsh.redeem.output;
 
-  const opts = { encoding: "utf8", mode: 0o600, flag: "a" };
+    // there coins can be hodl over years...
+    if (config.hodl_addr != p2wsh.address) {
+        console.log('#ERROR: mistmatch hold_addr, generated ['.brightRed + p2wsh.address + '] vs saved ['.brightRed + config.hodl_addr + ']'.brightRed);
+        return;
+    }
 
-  const msg = 'hodl at [' + config.hodl_addr + '] until [' + hodl_ts + '] return to [' + receiver + '], TXRAW=[' + txraw + "]\n";
 
-  fs.writeFileSync("txlist.txt", msg, opts);
+    const psbt = new bitcoin.Psbt( { network:network } );
+    psbt.setLocktime(config.lockTime);
+
+    psbt.addInput( { 
+        hash:config.txid, 
+        index:config.vout,   // 0 for first hodl tx, 1 for second... 
+        sequence:0xfffffffe,
+        witnessScript: ws_buff,
+        nonWitnessUtxo: Buffer.from(config.tx_hex, 'hex'),
+        redeemScript
+    });
+
+
+    var receiver = 'bc1qa5judde8fftgu92rv29vlelw0dpr66nh0wdnut'; // alpet donation addr
+    if (args[0])
+      receiver = args[0]; // override receiver
+    else
+      console.log("#WARN: using embedded receiver address ".brightRed + receiver.brightWhite);
+
+    var totalInput = config.total_hodl; // 1BTC
+
+    if (args[1])
+      totalInput = parseFloat(args[1]);
+
+    totalInput = Math.floor(totalInput * 100000000); // convert to Satoshis
+    var totalOutput = totalInput - 27000; // WARN: specify yours fee here
+
+    console.log ("#WARN: totalInput used ".red + totalInput.toString().brightMagenta + " sat.!".red);
+
+    psbt.addOutput({address:receiver, value:totalOutput}) // to whom and how much money to send?
+
+    const wsDump = execSync('bitcoin-cli decodescript ' + config.witness_script);
+    const pubkey = Buffer.from(keyPair.publicKey);  
+    const pubkey_hex = pubkey.toString('hex');
+    console.log("#WITNESS_SCRIPT: ".brightYellow + wsDump);    
+    if (wsDump.includes(pubkey_hex))
+        console.log("#OK: publicKey used in script".brightWhite);
+    else {
+        console.log("#FATAL: publicKey not used in script ".brightRed + pubkey_hex.brightWhite);
+        console.log(keyPair);
+        return;
+    }
+      
+    const signer = {
+        network: network,
+        publicKey: pubkey,      
+        sign: ($hash) => {   
+          return Buffer.from(keyPair.sign($hash)); 
+        },
+    };
+
+    psbt.signInput(0, signer);
+
+    const finalizeInput = (_inputIndex, input ) => {
+      const signature = Buffer.from(input.partialSig[0].signature);
+      console.log("#SIGNATURE: ".brightYellow + signature.toString('hex').brightGreen);
+
+      const redeemPayment = bitcoin.payments.p2wsh({
+          redeem: {
+            input: bitcoin.script.compile([signature]),
+            output: input.witnessScript
+          }
+        });
+
+        const result = psbtutils.witnessStackToScriptWitness (redeemPayment.witness ?? []);
+        return { finalScriptSig: Buffer.from(""), 
+                finalScriptWitness:result }
+    }
+
+    psbt.finalizeInput(0, finalizeInput);
+
+    const txraw = psbt.extractTransaction().toHex();
+    console.log('TXRAW="' + txraw.brightWhite + '"');
+
+    const opts = { encoding: "utf8", mode: 0o600, flag: "a" };
+
+    const msg = 'hodl at [' + config.hodl_addr + '] until [' + hodl_ts + '] return to [' + receiver + '], TXRAW=[' + txraw + "]\n";
+
+    fs.writeFileSync("txlist.txt", msg, opts);
--- sethodltx.js
+++ sethodltx.js
@@ -1,88 +1,87 @@
-const colors = require('colors');
-const fs = require('fs');
-config = require('./config.json');
-const { execSync } = require('child_process');
+    const colors = require('colors');
+    const fs = require('fs');
+    config = require('./config.json');
+    const { execSync } = require('child_process');
 
 
-const args = process.argv.slice(2);
-const txid = args[0];
-const wallet = args[1];
-if (!txid) {
-  console.log("must be specified TXID!");
-  return;
-}
-let txinfo = '';
-if (wallet)
-try  {
- txinfo = execSync('bitcoin-cli -rpcwallet=' + wallet + ' gettransaction ' + txid).toString(); 
- console.log(txinfo.yellow);
- txinfo = JSON.parse(txinfo);
- if (txinfo.details) {
-    const det = txinfo.details[0];
-    if (det.address != config.hodl_addr) {
-      console.log("#ERROR: address in transaction [".brightRed + det.address.brightWhite + "] != hodl address".brightRed);
+    const args = process.argv.slice(2);
+    const txid = args[0];
+    const wallet = args[1];
+    if (!txid) {
+      console.log("must be specified TXID!");
       return;
     }
-    config.txid = txid.trim();
-    config.tx_hex = det.hex;
-    config.vout = det.vout;
-    config.total_hodl = Math.abs(det.amount);
-    str = JSON.stringify(config);
-    console.log("Updated config: " + str.brightCyan);
-    fs.writeFileSync('./config.json', str);
-  }
-} 
-catch (err) {   
-  console.log("#EXCEPTION: " + err);
-}  
-else
-try {
-  // using raw TX required txindex=1 in bitcoin.conf 
-  txraw = execSync('bitcoin-cli getrawtransaction ' + txid).toString();
-  console.log("#TXRAW: " + txraw.length);
-  if (txraw.length < 400) 
-    throw new Error("getrawtransaction failed/small " + txraw);
-
-  txinfo = execSync("bitcoin-cli decoderawtransaction " + txraw);
-  txinfo = JSON.parse(txinfo);
-
-  if (txinfo.vin)
-   txinfo.vin.forEach(vin => { 
-      console.log("#CHECK_TX: " + vin.txid); //
-      if (vin.txid == txid && vin.txinwitness) { // possible never exec
-        config.witness_script = vin.txinwitness[1];
-        console.log("#OK: Witness script found ".brightWhite);
-      }
-    });
-   
-  assigned = false;
-  if (txinfo.vout)  
-   txinfo.vout.forEach(vout => { 
-   
-     console.log('#' + vout.n + ' addr: ' + vout.scriptPubKey.address);
-     if (vout.scriptPubKey.address == config.hodl_addr) {
-       config.vout  = vout.n;
-       config.txid = txid.trim(); 
-       config.tx_hex = txraw.replace(/^\s+|\s+$/g, ''); // remove \n
-       config.total_hodl = vout.value;
-       str = JSON.stringify(config);
-       console.log("Updated config: " + str.brightCyan);
-       fs.writeFileSync('./config.json', str);       
-       assigned = true;
-       return;
-     } 
+    let txinfo = '';
+    if (wallet)
+        try  {
+            txinfo = execSync('bitcoin-cli -rpcwallet=' + wallet + ' gettransaction ' + txid).toString(); 
+            console.log(txinfo.yellow);
+            txinfo = JSON.parse(txinfo);
+            if (txinfo.details) {
+                const det = txinfo.details[0];
+                if (det.address != config.hodl_addr) {
+                    console.log("#ERROR: address in transaction [".brightRed + det.address.brightWhite + "] != hodl address".brightRed);
+                    return;
+                }
+                config.txid = txid.trim();
+                config.tx_hex = det.hex;
+                config.vout = det.vout;
+                config.total_hodl = Math.abs(det.amount);
+                str = JSON.stringify(config);
+                console.log("#OK: Updated config: " + str.brightCyan);
+                fs.writeFileSync('./config.json', str);
+            }
+        } 
+        catch (err) {   
+            console.log("#EXCEPTION: " + err);
+        }  
     else
-     if (!assigned)
-        console.log("#REJECTED: ".brightRed + " due mismatch with hold_addr ");
-    
-  });
-  
+        try {
+            // using raw TX required txindex=1 in bitcoin.conf 
+            txraw = execSync('bitcoin-cli getrawtransaction ' + txid).toString();
+            console.log("#TXRAW: length = " + txraw.length.toString().brightMagenta);
+            if (txraw.length < 400) 
+                throw new Error("getrawtransaction failed/small " + txraw);
 
-}
-catch (err) { 
-  console.log("#EXCEPTION: " + err);
-}  
+            txinfo = execSync("bitcoin-cli decoderawtransaction " + txraw);
+            txinfo = JSON.parse(txinfo);
 
-console.log("Hodl address used: " + config.hodl_addr.brightWhite);
+            if (txinfo.vin)
+                txinfo.vin.forEach(vin => { 
+                    console.log("#CHECK_TX: ".brightYellow + vin.txid.brightGreen); //
+                    if (vin.txid == txid && vin.txinwitness) { // possible never exec
+                        config.witness_script = vin.txinwitness[1];
+                        console.log("#OK: Witness script found ".brightWhite);
+                    }
+                    });
+            
+            assigned = false;
+            if (txinfo.vout)  
+                txinfo.vout.forEach(vout => {         
+                    if (assigned) return;
+                    const n = vout.n;
+                    console.log('#CHECK: vout #'.brightYellow + n.toString().brightMagenta + ' addr: ' + vout.scriptPubKey.address);                    
+                    if (vout.scriptPubKey.address == config.hodl_addr) {
+                        config.vout  = n;
+                        config.txid = txid.trim(); 
+                        config.tx_hex = txraw.replace(/^\s+|\s+$/g, ''); // remove \n
+                        config.total_hodl = vout.value;
+                        str = JSON.stringify(config);
+                        console.log("#OK: Updated config: ".brightWhite + str.brightCyan);
+                        fs.writeFileSync('./config.json', str);       
+                        assigned = true;
+                        return;
+                    } 
+                    else                        
+                        console.log("#REJECTED: ".brightRed + " due  " + vout.scriptPubKey.address.brightWhite + " mismatch with hold_addr ");            
+            });
+        
+
+        }
+        catch (err) { 
+            console.log("#EXCEPTION: " + err);
+        }  
+
+    console.log( "#SUCCESS: ".brightGreen + "HODL address used ".brightYellow + config.hodl_addr.brightWhite + ", accepted TXID: ".brightYellow + txid.brightWhite);
 
 
